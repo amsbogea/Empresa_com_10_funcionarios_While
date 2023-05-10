7d5cb8be71aa7c4324be190df501f209b834cce5
Alex Miranda <alex_miranda@outlook.com.br>
2023-05-10 19:46:21 -0300

    Estruturando o projeto

---
+++ src/empresa_com_10_funcionarios_While/Funcionario.java
@@ -0,0 +1,5 @@
+package empresa_com_10_funcionarios_While;
+
+public class Funcionario {
+
+}
---
+++ src/empresa_com_10_funcionarios_While/Main.java
@@ -0,0 +1,10 @@
+package empresa_com_10_funcionarios_While;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
